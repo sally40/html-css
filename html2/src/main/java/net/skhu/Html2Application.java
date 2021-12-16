f318cd49f24b3ddb34cde9108c6310d5a8d988d8
seodahyun <seodahyun@218.38.137.27>
2021-12-16 20:12:11 +0900

    First Commit

---
+++ html2/src/main/java/net/skhu/Html2Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Html2Application.class, args);
+	}
+
+}
