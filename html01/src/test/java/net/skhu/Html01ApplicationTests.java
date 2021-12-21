c3c2da3f42295f477f0d82c8f3d877cab3bca0ca
seodahyun <seodahyun@218.38.137.28>
2021-12-21 14:34:28 +0900

    Second Commit

---
+++ html01/src/test/java/net/skhu/Html01ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Html01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
