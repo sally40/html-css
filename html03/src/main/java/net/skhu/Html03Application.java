78f6dcce6dac435309bf0e72b3070c93ac034a4a
seodahyun <seodahyun@218.38.137.28>
2021-12-21 16:34:36 +0900

    4th Commit

---
+++ html03/src/main/java/net/skhu/Html03Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Html03Application.class, args);
+	}
+
+}
---
+++ html03/src/test/java/net/skhu/Html03ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Html03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
