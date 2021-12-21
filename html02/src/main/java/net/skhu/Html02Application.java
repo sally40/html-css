3953159dc5c80411a016e41ce34267bc7b62f945
seodahyun <seodahyun@218.38.137.28>
2021-12-21 15:30:03 +0900

    Third Commit

---
+++ html02/src/main/java/net/skhu/Html02Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Html02Application.class, args);
+	}
+
+}
